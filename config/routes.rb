4a11a127be153f7de2644c910269484288c9f344
Ruby Rails Course <ivan.cavcic@hafiel.com>
2017-11-22 01:32:15 +0000

    Added bunch of stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -10,6 +9,8 @@ Rails.application.routes.draw do
   #get 'pages/home', to: 'pages#home'
   #get 'pages/about', to: 'pages#about'
   get 'about', to: 'pages#about'
+  
+  resources :articles
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
